0cc8a6e4167ae63117b00bab8b7b95363e91e8a3
Markus Ottela <oottela@cs.helsinki.fi>
2021-07-27 05:14:46 +0300

    1.21.08

--- src/common/statics.py
+++ src/common/statics.py
@@ -21,7 +21,7 @@ along with TFC. If not, see <https://www.gnu.org/licenses/>.
 
 """Program details"""
 TFC         = 'TFC'
-VERSION     = '1.21.06'
+VERSION     = '1.21.08'
 TRANSMITTER = 'Transmitter'
 RECEIVER    = 'Receiver'
 RELAY       = 'Relay'
